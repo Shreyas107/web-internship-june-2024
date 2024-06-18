229ae1c461611b5d02c2b47c34cc488d2e9426e3
Shreyas_Jadhav <shreyasjadhav107@gmail.com>
2024-06-18 10:07:51 +0530

    flask-jwt deleted

--- Day 10/flask-jwt/app/models.py
+++
@@ -1,9 +0,0 @@
-from .db import db
-
-# Defining a User model using 'db.Model', which represents a database table
-class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(150), nullable=False)
-    email = db.Column(db.String(150), unique=True, nullable=False)
-    password = db.Column(db.String(150), nullable=False)
-    created_at = db.Column(db.DateTime, server_default=db.func.now())
--- Day 10/flask-jwt/app/utils.py
+++
@@ -1,9 +0,0 @@
-import hashlib
-from flask import jsonify
-
-def create_response(message, status=200):
-    return jsonify({"message": message}), status
-
-# Function to hash a password using SHA-256 algorithm and return the hexadecimal digest
-def hash_password(password):
-    return hashlib.sha256(password.encode()).hexdigest()
